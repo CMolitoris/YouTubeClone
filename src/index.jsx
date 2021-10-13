05217f68cc722df19cd3ce44c80519831d127ba1
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2021-10-13 15:50:09 -0500

    File Structure for components and initial app.jsx clean up
    File Structure for components and initial app.jsx clean up

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import reactDom from 'react-dom';
+import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.css'
+
+reactDom.render(<App />, document.getElementById('root'));

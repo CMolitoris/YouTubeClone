58b4f6e553fc26fc0ed0b6be09c8b3de30751be1
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2021-10-14 11:44:21 -0500

    SearchBar Form
    SearchBar Form

--- src/components/Header/SearchBar/SearchBar.jsx
+++ src/components/Header/SearchBar/SearchBar.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col'
 
 
 const SearchBar = () => {
+    const [search, setSearch] = useState({
+        searchInput: ''
+    })
 
+    const handleChange = (event) => {
+        event.persist();
+        setSearch((search) => ({
+            [event.target.name]: event.target.value,
+        }));
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
 
     return ( 
-        <div className='col'>
-            --Search Bar--
-            <input className='form-control' type="text" name='searchBar'/>
-            <button type='button' className='btn btn-sm btn-danger'>Logo</button>
-        </div>
+        <Col>
+            <Form onSubmit={handleSubmit} className='get'>
+                <Col>
+                    <Form.Label>Logo</Form.Label>
+                </Col>
+                <Col>
+                    <Form.Control className='form-control' type="text" name='searchInput'  placeholder="Search..." onChange={handleChange}/>
+                </Col>
+                <Col>
+                    <Button type='submit' className='btn btn-sm btn-danger'>Logo</Button>
+                </Col>
+                
+            </Form>
+        </Col>
      );
 }
 
